2ab4c8fa8f983df6d260a768cbe91607e727f1f2
yvos77 <yuripb902@gmail.com>
2025-08-16 12:17:00 -0300

    Update Exercicio3Aula2.java

--- Atvd2 - POO/Exercicio3Aula2.java
+++ Atvd2 - POO/Exercicio3Aula2.java
@@ -1,19 +1,28 @@
-package exercicios_aula2;
-
 import java.util.Scanner;
 
 public class Exercicio3Aula2 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite 5 numeros separados: ");
-        String num = sc.nextLine();
-        sc.close();
-        String[] numeros = num.split(" ");
 
-        System.out.print("Numeros:\n");
-        for (int i=0; i < numeros.length; i++) {
-            System.out.printf("%s\n", numeros[i]);
+        int[] numeros = new int[5];
+
+        System.out.printf("Digite 5 números inteiros: \n");
+
+        for (int i=0; i<numeros.length; i++) {
+            System.out.printf("Número %d: ", i+1);
+            numeros[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        System.out.print("\nNúmeros digitados: ");
+        for (int i=0; i<numeros.length; i++) {
+            if (i<=3) {
+                System.out.printf("%d, ", numeros[i]);
+            } else {
+                System.out.printf("%d", numeros[i]);
+            }
         }
     }
 }
